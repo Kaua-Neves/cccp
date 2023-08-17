c2e6798460399ef39e71b10d983b86917c89d0cf
Kaua-Neves <110940361+Kaua-Neves@users.noreply.github.com>
2023-08-17 08:18:30 -0300

    sss

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+Lembre-se de personalizar o conteúdo das seções e substituir as imagens de exemplo pelos seus próprios recursos visuais. Este código irá criar uma página responsiva para dispositivos móveis, com um menu "hamburguer" para navegação em telas menores. Certifique-se de ter as imagens reais, como 'project1.jpg', 'project2.jpg', e adicione qualquer outra imagem que seja necessária.
+
+Além disso, você precisará de uma imagem para o menu "hamburguer", que pode ser um ícone de três linhas horizontais (geralmente chamado de ícone "hamburguer"). Você pode usar um ícone de fonte, uma imagem ou SVG para isso.
+
+Lembre-se de criar os arquivos styles.css e scripts.js e vinculá-los corretamente no seu arquivo HTML. Isso deve lhe dar uma base sólida para uma página responsiva e amigável para dispositivos móveis.
+
+
+
+
+
